7ed2127433d33f27dd2b72deee868d3402bb5eee
scorpion00100 <danbetheliryivuze@gmail.com>
2023-04-24 13:56:11 +0200

    modify link

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -17,7 +17,7 @@ $(function () {
   });
 });
 
-$.get('http://localhost:5001/api/v1/status/', function (data) {
+$.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
   console.log(data);
   if (data.status === 'OK') {
     $('div#api_status').addClass('available');
