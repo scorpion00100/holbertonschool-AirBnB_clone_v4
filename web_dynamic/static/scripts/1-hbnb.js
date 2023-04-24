771b57298cd84eff126b3208a085f9bbaebd2e14
scorpion00100 <danbetheliryivuze@gmail.com>
2023-04-24 11:57:19 +0200

    script must be executed only when DOM is loaded

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,18 @@
+
+$(function () {
+  const amenitiesChecked = {};
+  $(document).on('change', "input[type='checkbox']", function () {
+    if (this.checked) {
+      amenitiesChecked[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenitiesChecked[$(this).data('id')];
+    }
+    const objs = Object.values(amenitiesChecked);
+    console.log(Object.values(amenitiesChecked));
+    if (objs) {
+      $('.amenities > h4').text(Object.values(amenitiesChecked).join(', '));
+    } else {
+      $('.amenities > h4').html('&nbsp;');
+    }
+  });
+});
